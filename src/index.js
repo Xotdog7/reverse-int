91744def25f469df85b82fb3b3dc251f4ca30b41
Rustem <xotdog7777@gmail.com>
2021-03-16 00:03:40 +0600

    init: start reverse-int. And to solve the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    str = String(n)
+    razbivaem_massiv = str.split("");
   
+    Perevorachivaem_massiv = razbivaem_massiv.reverse();
+    Obedinaem_massiv = Perevorachivaem_massiv.join("");
+   return parseInt(Obedinaem_massiv) 
 }
+console.log(module.exports(123));
